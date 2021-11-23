a9f0c5248d219feb53ed9d71c8b18477becfeb99
arianna banks <ariannasbanks@gmail.com>
2021-11-22 20:01:13 -0500

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# password-generator
+# Password Generator Project
+
+## table of contents
+- [objective](objective)
+- [usage](usage)
+- [visuals](visuals)
+## objective
+- create a randomized password with requirements being choosing between 8-128 characters and at least 1 of 4 character options.
+
+## usage
+- given someone needs a new secure password, a user can click the button and go through the following propmpts to randomly generate one
+- requires user to choose a number between 8 and 128, otherwise returns "please choose a valid option"
+- creates variables for each case and makes user choose confirm or cancel to above questions
+- alerts user to choose at least one character type if they go through all questions and don't choose any
+
+## visuals
+![pgp](./assets/images/pg.png)
---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var charset = "";
+var blahLength = 0;
+var str = "";
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  charset = "";
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function rando() {
+  str = "";
+  for (var i = 0; i < blahLength; i++) {
+    str += charset[Math.floor(Math.random() * charset.length)];
+  }
+  console.log(str)
+}
+
+function generatePassword() {
+  blahLength = Number(prompt("how many characters would you like your password to be?"));
+  while (isNaN(blahLength) || blahLength < 8 || blahLength > 128) blahLength = Number(prompt("please choose a valid option. the length must be between 8-128 characters."));
+  // requires user to choose a number between 8 and 128, otherwise returns "please choose a valid option"
+
+  function characterSets() {
+    var upperCase = confirm("would you like to use uppercase letters?");
+    if (upperCase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase = confirm("would you like to use lowercase letters?");
+    if (lowerCase) charset += "abcdefghijklmnopqrstuvwxyz";
+    var numCase = confirm("would you like to use numbers?");
+    if (numCase) charset += "1234567890";
+    var speChar = confirm("would you like to use special characters?");
+    if (speChar) charset += "!@#$%^&*(){}[]=<>/,.";
+    // creates variables for each case and makes user choose confirm or cancel to above questions
+
+    if (!upperCase && !lowerCase && !numCase && !speChar) {
+      alert("please select at least one character type! :-)");
+      characterSets();
+      // alerts user to choose at least one character type if they go through all questions and don't choose any
+    }
+  }
+  characterSets();
+  rando();
+  console.log(str);
+  return str;
+}
